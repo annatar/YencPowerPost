f88838c00cf5b101a048eec93d415df712b01fb8
YPP Dev <ypp.dev@gmail.com>
2010-05-26 19:26:43 -0500

    Adding Belgian French DLL for testing

---
+++ ResourceDll_fr-BE/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// ResourceDll_fr-BE.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
